a633fc0096904f9d3299fa4539e9c87b1bc26bdb
michaelkoppen <mkoppen98@hotmail.com>
2017-08-29 16:24:31 +1000

    make sender take user input which is sent to the reader.

--- src/MulticastSender.java
+++ src/MulticastSender.java
@@ -2,12 +2,16 @@
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.net.DatagramPacket;
+import java.util.Scanner;
 
 public class MulticastSender {
     public static void main(String[] args) throws Exception {
-        String message = "hello world";
         InetAddress address = InetAddress.getByName("228.5.6.7");
         MulticastSocket socket = new MulticastSocket(49152);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a message to send");
+        String message = input.nextLine();
 
         socket.joinGroup(address);
         DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(),address, 49152);
